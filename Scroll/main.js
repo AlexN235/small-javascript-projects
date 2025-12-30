be5a570fefa0a687dc7ca4e3e2697ef7d71f1e39
Alex Nguyen <alex.nguyen235@gmail.com>
2025-12-29 18:09:34 -0800

    scroll project

--- Scroll/main.js
+++ Scroll/main.js
@@ -1,1 +1,46 @@
 // scroll project javascript
+
+const currDate = new Date();
+const date = document.querySelector('.date');
+
+date.innerHTML = currDate.getFullYear();
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function() {
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    
+    if(containerHeight === 0) {
+        linksContainer.style.height = linksHeight + "px";
+        console.log(linksHeight);
+    }
+    else {
+        linksContainer.style.height = 0;
+    }
+})
+
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener("scroll", function() {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    console.log(scrollHeight);
+    console.log(navHeight);
+    if(scrollHeight > navHeight) {
+        navbar.classList.add('fix-nav');
+    }
+    else {
+        navbar.classList.remove('fix-nav');
+    }
+    
+    if(scrollHeight > 300) {
+        topLink.classList.add("show-link");
+    }
+    else {
+        topLink.classList.remove("show-link");
+    }
+});
